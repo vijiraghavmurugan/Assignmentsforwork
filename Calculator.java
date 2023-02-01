2fa672dc871dba50b5dcf9feeda44639f2dbe2ad
vijiraghavmurugan <119110225+vijiraghavmurugan@users.noreply.github.com>
2023-02-01 21:18:35 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -9,19 +9,30 @@ public class Calculator {
 
 		System.out.println("Do you want to perform operation:");
 		System.out.println("Type Y or N");
-		
+
 		while (scan.hasNext()) {
-			
+
 			String ans = scan.nextLine();
 
 			if (ans.equalsIgnoreCase("Y")) {
 				System.out.println("Enter the 1st number");
-				float first = scan.nextFloat();
-				first=calc(first);
-				System.out.println(first);
-				System.out.println("Do you want to perform operation:");
+				float firstNumber = scan.nextFloat();
+				float resultNumber = calc(firstNumber);
+				if (resultNumber != 0) { // to check whether firstNumbe
+					String resultNumberAsString = Float.toString(resultNumber);
+					String[] decimalString = resultNumberAsString.split("\\.");
+					int afterDecimal = Integer.parseInt(decimalString[1]);
+					if (afterDecimal > 0) {
+						System.out.println(resultNumber);
+					} else {
+						System.out.println((int) resultNumber);
+
+					}
+
+				}
+				System.out.println("\nDo you want to perform operation:");
 				System.out.println("Type Y or N");
-				
+
 			} else if (ans.equalsIgnoreCase("N")) {
 				System.out.println("Program stopped");
 				System.exit(0);
@@ -32,13 +43,15 @@ public class Calculator {
 	public static float calc(float first) {
 
 		System.out.println("Enter the operator");
+		System.out.println(
+				"+ for Addition ; - for Subtraction ; * for Multiplication ; / for Division ; % for Remainder ; pow for Power ; per for Percentage ; = for equals");
 		String oper = scan.next();
-		if(oper.equals("=")) {
+		if (oper.equals("=")) {
 			return first;
 		}
 		System.out.println("Enter the number");
 		float second = scan.nextFloat();
-		float res=0;
+		float res = 0;
 		switch (oper) {
 		case "+":
 			res = add(first, second);
@@ -68,9 +81,12 @@ public class Calculator {
 			res = per(first, second);
 			calc(res);
 			break;
+		default:
+			System.out.println("\nEnter a valid operator");
+			res = 0;
 		}
 		return res;
-		
+
 	}
 
 	public static float per(float first, float second) {
